762fa31522b0a1f9b2fd98c1779154a07ec801c7
Jean C Terra <jeancarlosterra@hotmail.com>
2022-01-11 11:16:13 -0300

    Pagination layout

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,6 +1,8 @@
+import Pagination from "components/Navbar/Pagination";
+
 function Listing(){
     return(
-        <h1>Pagina de Listagem</h1>
+        <Pagination/>
     )
 }
 export default Listing;
