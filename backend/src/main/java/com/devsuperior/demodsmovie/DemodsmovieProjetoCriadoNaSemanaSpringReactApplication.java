279efae4e474dadcb0c8f0a1ff07dd9650e0b2dc
Jean C Terra <jeancarlosterra@hotmail.com>
2022-01-11 08:31:18 -0300

    Project created

---
+++ backend/src/main/java/com/devsuperior/demodsmovie/DemodsmovieProjetoCriadoNaSemanaSpringReactApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demodsmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemodsmovieProjetoCriadoNaSemanaSpringReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemodsmovieProjetoCriadoNaSemanaSpringReactApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devsuperior/demodsmovie/DemodsmovieProjetoCriadoNaSemanaSpringReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.demodsmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemodsmovieProjetoCriadoNaSemanaSpringReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
