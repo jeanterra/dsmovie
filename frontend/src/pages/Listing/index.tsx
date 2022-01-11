ce24af909c45fb9895d450ce1facfde504bde8a2
Jean C Terra <jeancarlosterra@hotmail.com>
2022-01-11 17:22:01 -0300

    Movie Card

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,8 +1,31 @@
+import MovieCard from "components/Navbar/MovieCard";
 import Pagination from "components/Navbar/Pagination";
 
-function Listing(){
-    return(
-        <Pagination/>
-    )
+function Listing() {
+    return (
+        <>
+            <Pagination />
+            <div className="container">
+                <div className="row">
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                    <div className="col-sm-6 col-lg-4 col-xl-3 mb-3">
+                        <MovieCard />
+                    </div>
+                </div>
+            </div>
+        </>
+
+    );
 }
 export default Listing;
